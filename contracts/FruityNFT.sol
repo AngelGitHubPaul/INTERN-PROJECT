cbf7182fcd00fa0bf69ce161f63debe320e013e6
Gers08 <gersontano@gmail.com>
2023-06-05 23:47:50 +0800

    AV-Tano NFT smart contract with metadata

--- contracts/FruityNFT.sol
+++ contracts/FruityNFT.sol
@@ -1,46 +1,43 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
 
+contract FruityNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    using Strings for uint256;
 
-contract FruityNFT is ERC721URIStorage, Ownable {
-    uint256 public mintPrice;
-    uint256 public totalSupply;
-    uint256 public maxSupply;
-    uint256 public maxPerWallet;
-    bool public isPublicMintEnabled;
-    string internal baseTokenUri;
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public constant maxSupply = 10;
+    uint256 public constant maxPerWallet = 1;
     mapping(address => uint256) public walletMints;
 
-    constructor() payable ERC721('FruityNFT', 'FN') {
-        mintPrice = 0.00 ether;
-        totalSupply = 0;
-        maxSupply = 500;
-        maxPerWallet = 1;
-        baseTokenUri = "https://ipfs.io/ipfs/bafybeiabmqfka5hric53a7yxapqklhuhou22vhdlchfnvy6padxuxmapyy/";
+    constructor() payable ERC721("FruityNFT", "FN") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://nftstorage.link/ipfs/bafybeibmzuf32cgb7rbai7hkr4hwew7vdildb6sjnoivgcq5aod4n423mi/";
     }
 
-    // this is the url that the opensea will call for the image of nft, we override it so that we can assign the baseTokenUri to this function
     function tokenURI(uint256 _tokenId) public view override returns (string memory) {
         // checks if the tokenId exists
         require(_exists(_tokenId), 'Token does not exist');
-        // then convert the json file name to its address on the blockchain (abi.encodePacked)
-        return string(abi.encodePacked(baseTokenUri, Strings.toString(_tokenId), ".json"));
+        
+        // Convert the tokenId to a string
+        string memory tokenIdStr = _tokenId.toString();
+
+        // Concatenate the base URL, tokenId, and ".json" extension
+        string memory tokenUri = string(abi.encodePacked(_baseURI(), tokenIdStr, ".json"));
+
+        return tokenUri;
     }
 
-    function mint(uint256 _quantity) public payable {
-        require(totalSupply + _quantity <= maxSupply, 'sold out');
-
-        // checks if the minting will exceed the mint limit count per wallet
-        require(walletMints[msg.sender] < maxPerWallet, 'you already reach the limit per wallet');
-
-        uint256 newTokenId = totalSupply + 1;
-        // safemint is a function inside the ERC721 contract
-        _safeMint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, tokenURI(newTokenId));
-        totalSupply++;
-        walletMints[msg.sender]++;
+    function safemint(address to) public {
+        uint256 tokenId = _tokenIdCounter.current() + 1;
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
     }
 }
