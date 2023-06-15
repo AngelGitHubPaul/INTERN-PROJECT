8e88f99c35cbfecaf0cb0d8683fe1f203ab19e94
AngelGitHubPaul <darkangel22blazer@gmail.com>
2023-06-15 13:21:11 +0800

    AV-Agnabo vercel serverless function bug fix

--- src/api/server.js
+++ src/api/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const router = require('./routes');
 const { connectToDatabase } = require('../api/database/connect');
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 3000 ;
 const app = express();
 
 app.use(express.json());
