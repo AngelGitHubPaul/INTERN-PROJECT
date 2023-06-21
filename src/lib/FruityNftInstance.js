d5ca1daa80d1d234f08fb83c78e13828e1545845
KaiKai-dev <jecalaycay17@gmail.com>
2023-06-21 10:08:58 +0800

    SW - Calaycay - deployed new minting contract

--- src/lib/FruityNftInstance.js
+++ src/lib/FruityNftInstance.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 import contractABI from '../FruityNFT.json';
 
-const contractAddress = '0x3dF34C7368A8AD1D3258d93a8e4Ba2b77f391EF7';
+const contractAddress = '0x9008EAFEccd99D48509F59511C73177Ca69cCdd7';
 export let contract;
 export let userAddress;
 
